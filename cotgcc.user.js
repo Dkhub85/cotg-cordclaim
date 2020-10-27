53d8deef1a8319d12b03ea09857443ff951f1f50
Dhruv Kumar <40730522+Dkhub85@users.noreply.github.com>
2020-10-27 14:48:35 +0530

    Update cotgcc.user.js

--- cotgcc.user.js
+++ cotgcc.user.js
@@ -11,4 +11,46 @@
 // @downloadURL https://raw.githubusercontent.com/DKhub85/cotg-cordclaim/main/cotgcc.user.js
 // ==/UserScript==
 
-var _0xae22=['open','toString','#playerName','message','Cord\x20Claim','subscribe','substring','application/json','connect','setRequestHeader','\x20claimed\x20by\x20','https://discord.com/api/webhooks/766166495411437578/xwt4o5KsEjh7MVe3xLluez9Q6keeCjdYOpfBsgBXJriK8WtNUnYvNtKI9IUIqt0hbJbM','text','chat','match','alliance','Content-type','send'];(function(_0x1bd8a3,_0xae223b){var _0x8bcb88=function(_0x5395f2){while(--_0x5395f2){_0x1bd8a3['push'](_0x1bd8a3['shift']());}};_0x8bcb88(++_0xae223b);}(_0xae22,0x9b));var _0x8bcb=function(_0x1bd8a3,_0xae223b){_0x1bd8a3=_0x1bd8a3-0x0;var _0x8bcb88=_0xae22[_0x1bd8a3];return _0x8bcb88;};(function(){var _0x22a283=_0x8bcb,_0x5395f2,_0xe8dc21,_0x31efa9;cotgsubscribe[_0x22a283('0xc')]('chat',function(_0x64f7a7){var _0x50413a=_0x22a283;_0x64f7a7['type']==_0x50413a('0x4')&&_0x93a0c3(_0x64f7a7);}),setTimeout(function(){var _0x1a8b56=_0x22a283;cotg[_0x1a8b56('0x2')][_0x1a8b56('0xf')]();},0x2710);function _0x93a0c3(_0x475bb7){var _0x37615f=_0x22a283;_0x31efa9=$(_0x37615f('0x9'))[_0x37615f('0x1')](),_0xe8dc21=_0x475bb7['player'];var _0x31854f=[];_0x31854f=_0x475bb7[_0x37615f('0xa')][_0x37615f('0x3')](/\d+/g),_0x5395f2=(_0x31854f[0x0]+':'+_0x31854f[0x1])[_0x37615f('0x8')](),_0x475bb7[_0x37615f('0xa')][_0x37615f('0xd')](0x0,0x6)=='iclaim'&&_0x31efa9==_0xe8dc21&&_0x46bac6();}function _0x46bac6(){var _0x185edf=_0x22a283,_0x3dc095=new XMLHttpRequest();_0x3dc095[_0x185edf('0x7')]('POST',_0x185edf('0x0')),_0x3dc095[_0x185edf('0x10')](_0x185edf('0x5'),_0x185edf('0xe'));var _0x31c4b5={'username':_0x185edf('0xb'),'avatar_url':'','content':_0x5395f2+_0x185edf('0x11')+_0xe8dc21};_0x3dc095[_0x185edf('0x6')](JSON['stringify'](_0x31c4b5));}}());
+(function() {
+    var message;
+    var name;
+    var gname;
+    cotgsubscribe.subscribe( "chat", function( data ) {
+        if(data.type=='alliance') {
+            processalliance(data);
+        }
+ //       return data;
+    },1);
+    //pause to make sure chat is connected then register chatbot
+    setTimeout(function() {
+        cotg.chat.connect();
+    }, 10000);
+
+
+    function processalliance(data) {
+        gname=$('#playerName').text();
+        name=data.player;
+        var messagex=[];
+        messagex = data.message.match(/\d+/g);
+        message = (messagex[0]+":"+messagex[1]).toString();
+        if(data.message.substring(0, 6)=="iclaim" && gname==name){
+            sendMessage();
+             //012345 &lt;coords&gt;349:252&lt;/coords&gt;!regex /\d+/g
+        }
+
+    }
+    function sendMessage() {
+        var request = new XMLHttpRequest();
+        request.open("POST", "https://discord.com/api/webhooks/766166495411437578/xwt4o5KsEjh7MVe3xLluez9Q6keeCjdYOpfBsgBXJriK8WtNUnYvNtKI9IUIqt0hbJbM");
+
+        request.setRequestHeader('Content-type', 'application/json');
+
+        var params = {
+            username: "Cord Claim",
+            avatar_url: "",
+            content: message +" claimed by "+ name
+        }
+
+        request.send(JSON.stringify(params));
+    }
+})();
